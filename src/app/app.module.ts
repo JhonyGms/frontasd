34e81dabb9543278b20db0ec58586945b9004fd6
JhonyGms <jhonatan.garcia.ayala.99@gmail.com>
2022-10-12 15:03:44 -0500

    definicion componenetes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { InformationComponent } from './components/information/information.component';
+import { FormEditComponent } from './components/form-edit/form-edit.component';
+import { FormCreateComponent } from './components/form-create/form-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    InformationComponent,
+    FormEditComponent,
+    FormCreateComponent
   ],
   imports: [
     BrowserModule,
